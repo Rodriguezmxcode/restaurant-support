5e68dc5e40881a14fb45a317b7d2bf30c02dfa46
Rodriguezmxcode <rodriguez.evolife@gmail.com>
2026-08-18 18:39:11 -0600

    Gate OpsVista app behind authenticated session

--- opsvista/src/main.tsx
+++ opsvista/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AuthGate from './AuthGate';
 import './styles.css';
 import './rampComplianceOverdue.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthGate><App /></AuthGate>
   </React.StrictMode>,
 );
