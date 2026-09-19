13c7c6b870cf827afa3b2ef9bbe0a1e1ad11a20f
Rodriguezmxcode <rodriguez.evolife@gmail.com>
2026-09-19 02:34:46 -0600

    Serve marketing site on getopsvista.com host

--- opsvista/src/main.tsx
+++ opsvista/src/main.tsx
@@ -4,14 +4,17 @@ import App from './App';
 import AppErrorBoundary from './AppErrorBoundary';
 import AuthGate from './AuthGate';
 import { I18nProvider } from './i18n';
+import MarketingSite from './MarketingSite';
 import './styles.css';
 import './maxDataTheme.css';
 import './rampComplianceOverdue.css';
 import './corporateImportBootstrap';
 import './priceWatchBootstrap';
 
+const isMarketingHost=typeof window!=='undefined'&&['getopsvista.com','www.getopsvista.com'].includes(window.location.hostname.toLowerCase());
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <I18nProvider><AppErrorBoundary><AuthGate><App /></AuthGate></AppErrorBoundary></I18nProvider>
+    <I18nProvider><AppErrorBoundary>{isMarketingHost?<MarketingSite/>:<AuthGate><App /></AuthGate>}</AppErrorBoundary></I18nProvider>
   </React.StrictMode>,
 );
