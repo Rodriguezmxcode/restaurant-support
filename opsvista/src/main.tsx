2c4021e869a848ffd6967304a98b075a7703d666
Rodriguezmxcode <rodriguez.evolife@gmail.com>
2026-09-18 23:29:34 -0600

    Wrap OpsVista in bilingual language provider

--- opsvista/src/main.tsx
+++ opsvista/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import AppErrorBoundary from './AppErrorBoundary';
 import AuthGate from './AuthGate';
+import { I18nProvider } from './i18n';
 import './styles.css';
 import './maxDataTheme.css';
 import './rampComplianceOverdue.css';
@@ -11,6 +12,6 @@ import './priceWatchBootstrap';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppErrorBoundary><AuthGate><App /></AuthGate></AppErrorBoundary>
+    <I18nProvider><AppErrorBoundary><AuthGate><App /></AuthGate></AppErrorBoundary></I18nProvider>
   </React.StrictMode>,
 );
